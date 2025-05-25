4bafa667d5cc52878f546d214a7538f38f3d6885
Daniel Chae <daniel.jiwoong@gmail.com>
2025-05-26 06:07:43 +0900

    chore: implement graceful shutdown pattern

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import path from "path";
+import type { Server } from "http";
 import apiRouter from "./routes/api";
 import hookRouter from "./routes/hook";
 import PostgresClient from "./controllers/postgresql";
@@ -8,16 +9,16 @@ import MongoClient from "./controllers/mongo";
 import { rawBodyParser } from "./middleware";
 import config from "./config/config";
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Middleware to store raw data in the request
-app.use(rawBodyParser);
-
-app.use(express.json());
-app.use(express.static("dist"));
-
 const initApp = async () => {
+  const app = express();
+  const PORT = process.env.PORT || 3000;
+
+  // Middleware to store raw data in the request
+  app.use(rawBodyParser);
+
+  app.use(express.json());
+  app.use(express.static("dist"));
+
   const pg = new PostgresClient(config.getPGConfig());
   const mongo = new MongoClient(config.getMongoConfig());
   await mongo.connectToDatabase();
@@ -31,9 +32,31 @@ const initApp = async () => {
     res.sendFile(path.join(__dirname, "../dist", "index.html"));
   });
 
-  app.listen(PORT, () => {
+  const server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+
+  // Graceful shutdown
+  process.on("SIGTERM", () => cleanup(server, pg, mongo));
+  process.on("SIGINT", () => cleanup(server, pg, mongo));
+
+  return { app, server, pg, mongo };
 };
 
-initApp();
+const cleanup = async (
+  server: Server,
+  pg: PostgresClient,
+  mongo: MongoClient,
+) => {
+  console.log("Shutting down...");
+  server.close();
+  await mongo.closeConnection();
+  await pg.disconnect();
+  process.exit(0);
+};
+
+// Start the app server
+initApp().catch((err) => {
+  console.error("Failed to start:", err);
+  process.exit(1);
+});
