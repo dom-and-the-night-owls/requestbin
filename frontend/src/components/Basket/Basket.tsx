984ad7c8944a78c7695e9db3d0361363dbed0579
Daniel Chae <daniel.jiwoong@gmail.com>
2025-06-08 09:21:43 +0900

    feat: added severity to toolbar action notifications

--- frontend/src/components/Basket/Basket.tsx
+++ frontend/src/components/Basket/Basket.tsx
@@ -40,6 +40,7 @@ const Basket = ({ originURL }: BasketProps) => {
     await navigator.clipboard.writeText(`${originURL}/hook/${basketName}`);
     notifications.show("Basket URL copied to clipboard", {
       key: "clipboard",
+      severity: "info",
       autoHideDuration: 2000,
     });
   };
@@ -49,6 +50,7 @@ const Basket = ({ originURL }: BasketProps) => {
 
     notifications.show("Formatted JSON", {
       key: "format",
+      severity: "info",
       autoHideDuration: 2000,
     });
   };
@@ -61,6 +63,7 @@ const Basket = ({ originURL }: BasketProps) => {
 
       notifications.show(`Deleted basket /${basketName}`, {
         key: "delete",
+        severity: "success",
         autoHideDuration: 2000,
       });
     } catch (error: unknown) {
@@ -75,6 +78,7 @@ const Basket = ({ originURL }: BasketProps) => {
 
     notifications.show(`Basket "${basketName}" successfully cleared.`, {
       key: "clear",
+      severity: "success",
       autoHideDuration: 2000,
     });
   };
@@ -84,6 +88,7 @@ const Basket = ({ originURL }: BasketProps) => {
 
     notifications.show(`Reversed list order`, {
       key: "reverse",
+      severity: "info",
       autoHideDuration: 2000,
     });
   };
